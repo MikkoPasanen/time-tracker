a72e81c6e8ab841c25a59d6de069a556cde53256
MikkoPasanen <113339319+MikkoPasanen@users.noreply.github.com>
2023-12-11 14:08:42 +0200

    Add hamburger menu to navbar on smaller screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,43 +2,89 @@
 import "../styles/navbar.css";
 import { NavLink } from "react-router-dom";
 import { useSettings } from "../components/SettingsContext";
+import { useState } from "react";
 
 export default function Navbar() {
     const { darkMode } = useSettings();
+    const [activeMenu, setActiveMenu] = useState(false);
     return (
         <nav className="navbar" theme={darkMode ? "dark-theme" : "light-theme"}>
-            <ul>
-                <li>
-                    <NavLink
-                        to="/"
-                        style={({ isActive }) => ({
-                            textDecoration: isActive ? "underline" : "none",
-                        })}
-                    >
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/about"
-                        style={({ isActive }) => ({
-                            textDecoration: isActive ? "underline" : "none",
-                        })}
-                    >
-                        About
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/settings"
-                        style={({ isActive }) => ({
-                            textDecoration: isActive ? "underline" : "none",
-                        })}
-                    >
-                        Settings
-                    </NavLink>
-                </li>
-            </ul>
+            <div className="normal-navbar">
+                <ul>
+                    <li>
+                        <NavLink
+                            to="/"
+                            style={({ isActive }) => ({
+                                textDecoration: isActive ? "underline" : "none",
+                            })}
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/about"
+                            style={({ isActive }) => ({
+                                textDecoration: isActive ? "underline" : "none",
+                            })}
+                        >
+                            About
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/settings"
+                            style={({ isActive }) => ({
+                                textDecoration: isActive ? "underline" : "none",
+                            })}
+                        >
+                            Settings
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="hamburger-navbar">
+                <button className={activeMenu ? "menu-btn active" : "menu-btn"} onClick={() => setActiveMenu(!activeMenu)}>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                    <span className="line"></span>
+                </button>
+                <div className="hamburger-content">
+                    <ul>
+                        <li>
+                            <NavLink
+                                to="/"
+                                style={({ isActive }) => ({
+                                    textDecoration: isActive ? "underline" : "none",
+                                })}
+                            >
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/about"
+                                style={({ isActive }) => ({
+                                    textDecoration: isActive ? "underline" : "none",
+                                })}
+                            >
+                                About
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/settings"
+                                style={({ isActive }) => ({
+                                    textDecoration: isActive ? "underline" : "none",
+                                })}
+                            >
+                                Settings
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     );
 }
